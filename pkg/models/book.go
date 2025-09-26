e7583ca67ab4a019bb2f96993bc31864e7adffea
Nafis Al Shams <nafisalshams@gmail.com>
2025-09-26 06:29:24 +0600

    modified app.go, utils.go, bookstore-routes.go

---
+++ pkg/models/book.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/nafisALshams/book-mgmt-system-go/pkg/config"
+)
+
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+
+func init() {
+	config.Connect()
+	db = config.GetDb()
+	db.AutoMigrate(&Book{})
+}
+
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import(
+	"github.com/gorilla/mux"
+	"github.com/nafisALshams/book-mgmt-system-go/pkg/controllers"
+)
+
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/books/", controllers.GetAllBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
