d872181f3b686849a5de43bcac6928fb5f9262dd
Nafis Al Shams <nafisalshams@gmail.com>
2025-09-26 06:42:00 +0600

    updated main.go and models/book.go

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -5,16 +5,14 @@ import (
 	"github.com/nafisALshams/book-mgmt-system-go/pkg/config"
 )
 
-
 var db *gorm.DB
 
 type Book struct {
 	gorm.Model
-	Name string `gorm:""json:"name"`
-	Author string `json:"author"`
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
-
 
 func init() {
 	config.Connect()
@@ -22,3 +20,26 @@ func init() {
 	db.AutoMigrate(&Book{})
 }
 
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find((&Books))
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
